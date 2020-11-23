94ba3bc777f77b07996c590c7e28c1335f2f3b00
SerhiiRuban81 <ruban_sa@hotmail.com>
2020-11-23 12:17:07 +0200

    component for displayng of todoItems array added

---
+++ AngularStrucutureDirectivesExample/src/app/mock-todo-items.ts
@@ -0,0 +1,10 @@
+import { from } from "rxjs";
+import {ToDoItem} from "./to-do-item";
+export const ITEMS: ToDoItem[] = [
+    {id:1, name: "Позвонить Ивану", isCompleted: false },
+    {id:2, name: "Выполнить ДЗ по Angular", isCompleted: false},
+    {id:3, name: "Подготовить презентацию", isCompleted: false},
+    {id:4, name: "Сделать покупки", isCompleted: false},
+    {id:5, name: "Выйти на прогулку", isCompleted: false}
+
+]
---
+++ AngularStrucutureDirectivesExample/src/app/to-do-items/to-do-items.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {ToDoItem} from "../to-do-item";
+import {ITEMS} from "../mock-todo-items"
+@Component({
+  selector: 'app-to-do-items',
+  templateUrl: './to-do-items.component.html',
+  styleUrls: ['./to-do-items.component.css']
+})
+export class ToDoItemsComponent implements OnInit {
+  items = ITEMS;
+  toDoItem: ToDoItem = {
+    id: 1 ,
+    name: "Закончить проект по автостеллам",
+    isCompleted: false
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
